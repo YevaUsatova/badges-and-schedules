a05f409fce09e50acd2e7686ede6c06ba9d457d0
YevaUsatova <yeva.usatova@gmail.com>
2021-03-05 00:29:10 -0500

    commit tree 503c29f789b48543c13923d8f15e4521322286c5

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def badge_maker (name)
+    return "Hello, my name is #{name}."
+end    
+def batch_badge_creator(name)
+    badge = []
+    name.each do |names|
+        badge << "Hello, my name is #{names}."
+    end
+    badge
+end    
+def assign_rooms (name)
+    rooms = []
+    name.each_with_index do |names, index|
+        index += 1
+        rooms << "Hello, #{names}! You'll be assigned to room #{index}!" 
+    end
+    rooms
+end  
+def printer(name)
+    badge = batch_badge_creator(name)
+    rooms = assign_rooms (name)
+    badge.each do |names|
+        puts names
+    end    
+    rooms.each do |names|
+       puts names    
+    end  
+end      
